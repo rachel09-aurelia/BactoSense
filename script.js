89a13bfbeab51d80ba4f2a8e6ced670f4a8dc7e6
rachel09-aurelia <chaureliar99@gmail.com>
2026-05-18 00:31:51 +0700

    BactoSense
    Website fixed

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const button = document.getElementById('exploreBtn');
 
-button.addEventListener{'click', () => {
+button.addEventListener('click', () => {
     document.getElementById('about').scrollIntoView({
         behavior: 'smooth'
     });
 });
+
+const text ="Innovating Microbiology Testing";
+const typingElement = document.querySelector('.typing-text');
+
+let index = 0;
+
+function typeText() {
+
+    if(index < text.length) {
+        typingElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeText, 80);
+    }
+}
+
+typeText();
+function openModal () {
+    document.getElementById("reportModal").style.display = "block";
+}
+function closeModal () {
+    document.getElementById("reportModal").style.display = "none";
+}
+function openOrderModal() {
+    document.getElementById("orderModal").style.display = "block";
+}
+function closeOrderModal() {
+    document.getElementById("orderModal").style.display = "none";
+}
